cbe622fac08f2220a56368ae9f7acf37db9488e9
nshorty <andrysciortino@gmail.com>
2021-10-05 15:40:15 +0200

    twolip: use Redfin's October FP

--- revengeos_twolip.mk
+++ revengeos_twolip.mk
@@ -47,3 +47,8 @@ PRODUCT_MODEL := Redmi Note 6 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := tulip
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.21101.001.A1 7641976 release-keys"
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.21101.001.A1/7641976:user/release-keys"
